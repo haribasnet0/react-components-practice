e30dda864732516016ccec1bfc22b06530036c95
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-04-24 11:34:19 +0300

    boiler plate added

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header'
+import Features from './Features'
+import RecentWorks from './RecentWorks'
+import Reviews from './Reviews'
+import Footer from './Footer'
+
+
+const  App = () =>{
+
+    return (
+        <div>
+            <Header />
+            <Features />
+            <RecentWorks />
+            <Reviews />
+            <Footer />
+        </div>
+    )
+}
+
+
+export default App;
---
+++ src/components/RecentWorks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecentWorks = () => {
+    return (
+        <div>
+            <h1>Recent Works</h1>
+        </div>
+    )
+}
+
+export default RecentWorks;
